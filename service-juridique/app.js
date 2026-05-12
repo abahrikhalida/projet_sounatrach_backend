a389ae90d3e056c052120299c01433f1d799f56d
lamia-kasmi <l.kasmi@esi-sba.dz>
2026-05-12 14:21:18 +0100

    back param

--- service-juridique/app.js
+++ service-juridique/app.js
@@ -6,6 +6,12 @@ const directionRoutes   = require('./routes/directionRoutes');
 const departementRoutes = require('./routes/departementRoutes');
 const errorHandler      = require('./middleware/errorHandler');
 const activiteRoutes = require('./routes/activiteRoutes');
+const directions_centrales= require('./routes/directionCentraleRoutes');
+const structureRoute = require('./routes/structureRoutes');
+const divisionRoute= require('./routes/divisionRoutes');
+const direction_activiteRoutes = require('./routes/directionActiviteRoutes');
+const departemet_activiteRoute= require('./routes/departementActiviteRoutes');
+
 
 
 
@@ -21,6 +27,11 @@ app.get('/info',   (req, res) => res.json({ app: 'service-juridique' }));
 app.use('/juridique/directions',   directionRoutes);
 app.use('/juridique/departements', departementRoutes);
 app.use('/juridique/activites', activiteRoutes);
+app.use('/juridique/directions-centrales',directions_centrales)
+app.use('/juridique/structure',   structureRoute);
+app.use('/juridique/division', divisionRoute);
+app.use('/juridique/direction_activite', direction_activiteRoutes);
+app.use('/juridique/departemet_activite',departemet_activiteRoute)
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: `Route ${req.originalUrl} non trouvée` });
